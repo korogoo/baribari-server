9e7840254de720eaffa566ef74c311375c874b6c
JinGoo <zzgg3040@naver.com>
2026-05-18 18:13:26 +0900

    chore: 프로제트 초기  세팅

---
+++ src/main/java/com/twin/baribari/BaribariApplication.java
@@ -0,0 +1,13 @@
+package com.twin.baribari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaribariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaribariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twin/baribari/BaribariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twin.baribari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaribariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
