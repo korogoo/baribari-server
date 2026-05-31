b8cae8762bd8102a2ce45f16873583623143df49
JinGoo <zzgg3040@naver.com>
2026-05-31 23:15:49 +0900

    refactor: 불필요한 nested 제거

--- src/test/java/com/twin/baribari/member/domain/MemberRepositoryTest.java
+++ src/test/java/com/twin/baribari/member/domain/MemberRepositoryTest.java
@@ -3,8 +3,6 @@ package com.twin.baribari.member.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.twin.baribari.member.infrastructure.MemberRepositoryImpl;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,25 +15,20 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Nested
-    @DisplayName("회원 정보를 저장한다")
-    class Save {
+    @Test
+    void 회원_정보를_저장한다() {
+        // given
+        Member member = new Member(
+            "name",
+            "baribari123@gmail.com",
+            LoginProvider.GOOGLE,
+            "socialId"
+        );
 
-        @Test
-        void 회원_정보를_저장한다() {
-            // given
-            Member member = new Member(
-                "name",
-                "baribari123@gmail.com",
-                LoginProvider.GOOGLE,
-                "socialId"
-            );
+        // when
+        final long savedId = memberRepository.save(member);
 
-            // when
-            final long savedId = memberRepository.save(member);
-
-            // then
-            assertThat(savedId).isNotNull();
-        }
+        // then
+        assertThat(savedId).isNotNull();
     }
 }
