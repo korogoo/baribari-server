8a82d3c6e9b2edfb9780f038c06d74a2fbffb7e8
JinGoo <zzgg3040@naver.com>
2026-06-11 19:52:27 +0900

    fix: 헤더기반이기 때문에 csrf 비활성화 & session 비활성화 & 로그인/토큰재발급 기능 접근 허용

--- src/test/java/com/twin/baribari/post/presentation/PostControllerTest.java
+++ src/test/java/com/twin/baribari/post/presentation/PostControllerTest.java
@@ -28,11 +28,13 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @Transactional
+@WithMockUser
 class PostControllerTest {
 
     @Autowired
